a4a3b23cef6c88eb05e89a7f320fdf652738a911
elizabethrsotomayor <elizabethrsotomayor@gmail.com>
2022-12-19 14:33:11 -0600

    clean comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,32 @@
 const ratings = document.getElementsByName("ratings");
-const ratings_form = document.getElementById("ratings-form");
 const submitBtn = document.getElementById("submit-btn");
 const ratings_card = document.getElementById("ratings-card");
 const thankyou_card = document.getElementById("thank-you-card");
 const user_rating = document.getElementById("user-rating");
 
-//console.log(ratings_form.action);
-
 ratings.forEach((el) => {
-  // console.log(el);
   el.addEventListener("click", function (e) {
-    // console.log(e.target.checked);
-    let cur_rating = e.target.id.split("-");
-    console.log(cur_rating);
+    let ratings_arr = e.target.id.split("-"); // [ratings,y]
+    let cur_rating = ratings_arr[cur_rating.length - 1];
 
-    user_rating.innerText = cur_rating[cur_rating.length - 1];
+    // To set value for "You selected __ out of 5"
+    user_rating.innerText = num_value;
+    user_rating.value = num_value;
   });
 });
 
 submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  // console.log(e.target.id);
-  changeState();
+
+  // If user_rating == undefined then user has not selected a value
+  if (user_rating.value == undefined) {
+    alert("Please select a value!");
+  } else {
+    changeState();
+  }
 });
 
 function changeState(form) {
   ratings_card.style.visibility = "hidden";
   thankyou_card.style.visibility = "visible";
-}
-
-// submitBtn.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   console.log(e);
-//   if ((e.target.classList = "submit-btn")) {
-//     console.log(e.target.innerText);
-//   }
-// });
-
-// function changeState(form) {
-//   console.log(form.action);
-//   form.action = "index.html";
-// }
-
-// ratings.forEach((el) => {
-//   console.log(el);
-// });
-
-// console.log(ratings);
-// for (let i = 0; i < ratings.length; i++) {
-//   console.log(ratings[i]);
-//   console.log(ratings[i].checked);
-// }
-
-function retrieveRating() {
-  for (let i = 0; i < ratings.length; i++) {
-    if (ratings[i].checked) console.log(ratings[i]);
-  }
 }
