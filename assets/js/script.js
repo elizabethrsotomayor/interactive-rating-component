0a4af9c0bff5471c97f1d63433557cbec74b5a45
elizabethrsotomayor <elizabethrsotomayor@gmail.com>
2022-12-19 17:16:06 -0600

    add mobile responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,16 @@ const submitBtn = document.getElementById("submit-btn");
 const ratings_card = document.getElementById("ratings-card");
 const thankyou_card = document.getElementById("thank-you-card");
 const user_rating = document.getElementById("user-rating");
+let cur_rating;
 
 ratings.forEach((el) => {
   el.addEventListener("click", function (e) {
     let ratings_arr = e.target.id.split("-"); // [ratings,y]
-    let cur_rating = ratings_arr[cur_rating.length - 1];
+    let cur_rating = ratings_arr[ratings_arr.length - 1];
 
     // To set value for "You selected __ out of 5"
-    user_rating.innerText = num_value;
-    user_rating.value = num_value;
+    user_rating.innerText = cur_rating;
+    user_rating.value = cur_rating;
   });
 });
 
